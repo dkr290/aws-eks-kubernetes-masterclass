7742647e02eaa517d656a0b0d25debe792b1b2ea
dani <d_krasimirov@yahoo.com>
2021-11-29 21:45:35 +0100

    adding in fargate not ssl LB test

--- 01-EKS-Create-Cluster-using-eksctl/01-01-Install-CLIs/ekscli-scripts/deploy-eks-existing-vpc.sh
+++ 01-EKS-Create-Cluster-using-eksctl/01-01-Install-CLIs/ekscli-scripts/deploy-eks-existing-vpc.sh
@@ -3,6 +3,12 @@
 #deploy with atomatic VPC pelase refer to the yaml to deploy with existing VPC
 eksctl create cluster --name=eksdemo1 \
                       --region=eu-central-1 \
-                      --vpc-private-subnets=subnet-0fcaed4ab8d710574,subnet-043f7d85b8ef5ba10 \
-                      --vpc-public-subnets=subnet-0939ed04e6646bd0d,subnet-092d2e9f572efebf9 \
+                      --vpc-private-subnets=subnet-0751e6722a6fd51f0,subnet-019d86a1b60152405 \
+                      --vpc-public-subnets=subnet-0eddfa7c33f6eb916,subnet-0bb0c596c6db7b089 \
+                      -- 
                       --without-nodegroup 
+
+#create tags
+#kubernetes.io/cluster/<name> tag set to either shared or owned
+#kubernetes.io/role/internal-elb tag set to 1 for private subnets
+#kubernetes.io/role/elb tag set to 1 for public subnets
